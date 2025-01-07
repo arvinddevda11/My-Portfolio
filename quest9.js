6aa084e43a6f389b439dbbab1434d13cde68193b
arvinddevda11 <arviipal42@gmail.com>
2025-01-07 12:19:16 +0530

    conditionals

---
+++ quest9.js
@@ -0,0 +1,14 @@
+// 9. **Check Even or Odd:**
+//    Write a script to:
+//    - Declare a variable `number = 23`.
+//    - Use a ternary operator to check and print:
+//      - `"Even"` if the number is even.
+//      - `"Odd"` if the number is odd.
+
+// ---
+
+       let number = 23;
+       
+       number % 2 ===0 ? console.log('Even') : console.log('Odd');
+        
+        // output is Odd
